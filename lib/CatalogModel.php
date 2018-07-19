4763c00e0e4b23caa2b70f740c905059a52a6451
Alex Yudenkov <ekytza@gmail.com>
2018-07-19 14:09:28 +0300

    fix

--- lib/CatalogModel.php
+++ lib/CatalogModel.php
@@ -1,50 +1,6 @@
 <?php
 namespace SampleApplication;
 
-trait DBSettings
-{
-    /**
-        * @var array Database connection settings
-    */
-    protected static $dsn = array(
-        'user'    => 'test',
-        'pass'    => 'test',
-        'db'      => 'test'
-    );
-
-    /**
-        * @var string Database table prefix
-    */
-    protected static $tablePrefix = 'tz_';
-
-    /**
-        * @var mixed Database connection pointer
-    */
-    protected static $dbConn = null;
-
-    /**
-         * Database settings getter
-         *
-         * @param string option name
-         * @return string|null
-    */
-    public static function getDBSettings($var)
-    {
-        if (isset(static::$$var))    return static::$$var;
-        else return null;
-    }
-
-    /**
-         * Database connection singleton
-         *
-         * @return mixed Database connection pointer
-    */
-    public static function getInstance()
-    {
-        return (static::$dbConn) ? static::$dbConn : static::$dbConn = new \SafeMySQL(static::$dsn);
-    }
-}
-
 interface CatalogModelInterface
 {
     /**
